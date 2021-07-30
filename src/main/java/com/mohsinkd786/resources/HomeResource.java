f8a609b7c55ada5e65ebb62e6e74765a3ac699c9
techmomo <mohsin.khursheed@techjini.com>
2021-07-30 10:11:03 +0530

    Greetings Api

--- src/main/java/com/mohsinkd786/resources/HomeResource.java
+++ src/main/java/com/mohsinkd786/resources/HomeResource.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RequestMapping("/home")
 @RestController
 public class HomeResource {
@@ -12,5 +15,10 @@ public class HomeResource {
     @GetMapping
     public ResponseEntity<String> message(){
         return ResponseEntity.ok("Spring Jenkins ");
+    }
+
+    @GetMapping("/greetings")
+    public ResponseEntity<List<String>> messages(){
+        return ResponseEntity.ok(Arrays.asList("Hello","Hola","Hi"));
     }
 }
